43854207c4c04399f5a37b1c1497e1b953b6c4e1
Anonik-Host <115616441+Anonik-dev@users.noreply.github.com>
2024-07-21 07:39:11 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7411742480:AAExg4pIcRksXfy-blpf3ZY8lrWi7lb7iH4'
+ADMIN_ID = '1713787118'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
